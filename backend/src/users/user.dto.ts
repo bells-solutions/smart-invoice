1c6bdfb67d0b53a3006b7f3cf5e5ab83b06ada24
Johan Bell <belljohan3@gmail.com>
2025-10-07 17:41:35 +0100

    feat: add UpdateUserDto class for user update functionality

---
+++ backend/src/users/user.dto.ts
@@ -0,0 +1,43 @@
+import { IsOptional, IsString } from "class-validator";
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  town?: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+  @IsOptional()
+  @IsString()
+  companyName?: string;
+
+  @IsOptional()
+  @IsString()
+  taxpayerNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  commercialRegister?: string;
+
+  @IsOptional()
+  @IsString()
+  poBox?: string;
+
+  @IsOptional()
+  @IsString()
+  companyLogo?: string;
+}
